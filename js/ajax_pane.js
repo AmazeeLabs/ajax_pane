8a63fb58707e673a35736094ca08f1c34a59dbac
creaoy <jk@arsum.com>
2012-10-05 19:26:47 +0300

    Adding module, js and plugins to 7.x branch

---
+++ js/ajax_pane.js
@@ -0,0 +1,18 @@
+//Handle AJAX panes
+
+  var AJAXPaneLoad = function (id, URL) {
+    var el = jQuery('#' + id);
+    if (el.length == 1 && URL) {
+      jQuery.ajax({
+        url: URL,
+        type: 'GET',
+        dataType: 'html',
+        success: function (res) {
+          el.after(res);
+          el.remove();
+        }
+      });
+    }
+  }
+
+
