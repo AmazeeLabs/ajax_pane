561ba97df324377fd32266656c56f86a210da40e
Chris Leppanen <chris.leppanen@gmail.com>
2012-11-11 19:54:32 -0800

    Code style fixes.

--- js/ajax_pane.js
+++ js/ajax_pane.js
@@ -1,18 +1,18 @@
-//Handle AJAX panes
+/**
+ * Handles AJAX panes.
+ */
+var AJAXPaneLoad = function (id, URL) {
+  var el = jQuery('#' + id);
 
-  var AJAXPaneLoad = function (id, URL) {
-    var el = jQuery('#' + id);
-    if (el.length == 1 && URL) {
-      jQuery.ajax({
-        url: URL,
-        type: 'GET',
-        dataType: 'html',
-        success: function (res) {
-          el.after(res);
-          el.remove();
-        }
-      });
-    }
+  if (el.length == 1 && URL) {
+    jQuery.ajax({
+      url: URL,
+      type: 'GET',
+      dataType: 'html',
+      success: function (res) {
+        el.after(res);
+        el.remove();
+      }
+    });
   }
-
-
+}
