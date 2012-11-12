024e45a5feeb9a7b68e26a74ce30ac180daa7519
Chris Leppanen <chris.leppanen@gmail.com>
2012-11-12 00:05:53 -0800

    Remove inline js. Use Drupal behaviors.

--- js/ajax_pane.js
+++ js/ajax_pane.js
@@ -1,18 +1,27 @@
-/**
- * Handles AJAX panes.
- */
-var AJAXPaneLoad = function (id, URL) {
-  var el = jQuery('#' + id);
+(function ($) {
 
-  if (el.length == 1 && URL) {
-    jQuery.ajax({
-      url: URL,
-      type: 'GET',
-      dataType: 'html',
-      success: function (res) {
-        el.after(res);
-        el.remove();
-      }
-    });
-  }
-}
+  /**
+   * Load Panels panes via ajax.
+   */
+  Drupal.behaviors.ajaxPaneLoad = {
+    attach: function (context, settings) {
+
+      $('.ajax-pane').once('ajax-pane-processed', function() {
+
+        var element = jQuery(this);
+        var url = element.attr('src');
+
+        jQuery.ajax({
+          url: url,
+          type: 'GET',
+          dataType: 'html',
+          success: function (response) {
+            element.replaceWith(response);
+          }
+        });
+      });
+
+    }
+  };
+
+})(jQuery);
