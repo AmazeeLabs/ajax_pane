ab6f015a05cc54194e7402857ce5694ab301edd3
Vasile Chindris <vasile.chindris@amazee.com>
2013-01-26 12:36:53 +0100

    Added the ajax refresh functionality.

--- js/ajax_pane.js
+++ js/ajax_pane.js
@@ -10,18 +10,28 @@
 
         var element = jQuery(this);
         var url = element.attr('src');
-
-        jQuery.ajax({
-          url: url,
-          type: 'GET',
-          dataType: 'html',
-          success: function (response) {
-            element.replaceWith(response);
-          }
-        });
+        var time = element.attr('rt');
+        update_ajax_pane(element, url);
+        // If we have to refresh the panel content, then set a time interval.
+        if (time) {
+          setInterval(function(){ update_ajax_pane(element, url); }, time * 1000);
+        }
       });
 
     }
   };
+  
+  function update_ajax_pane(element, url) {
+	jQuery.ajax({
+	  url: url,
+	  type: 'GET',
+	  dataType: 'html',
+	  success: function (response) {
+		// @todo: would be nice if we could use replaceWith() and not html().
+	    // element.replaceWith(response);
+	    element.html(response);
+	  }
+	});
+  }
 
 })(jQuery);
