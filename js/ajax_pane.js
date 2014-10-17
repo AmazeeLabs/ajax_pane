3a364b110f65e9fe0f9768123cef342b30e5b8c0
Vasile Chindris <vasile.chindris@amazee.com>
2014-10-17 14:23:19 +0200

    SRFESC-24 , SRFESC-11 : use the drupal ajax system to deliver the results of the ajax panes.

--- js/ajax_pane.js
+++ js/ajax_pane.js
@@ -5,23 +5,7 @@
    */
   Drupal.behaviors.ajaxPaneLoad = {
     attach: function (context, settings) {
-
-      $('.ajax-pane').once('ajax-pane-processed', function() {
-
-        var element = jQuery(this);
-        var url = element.attr('src');
-
-        jQuery.ajax({
-          url: url,
-          type: 'GET',
-          dataType: 'html',
-          success: function (response) {
-            element.replaceWith(response);
-            Drupal.attachBehaviors();
-          }
-        });
-      });
-
+	  $('.ajax-pane').trigger('click');
     }
   };
 
